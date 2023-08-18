1d38bb78728afbe519eb4d129fe1decae8b6e6fb
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-08-18 22:26:08 +0300

    Add the tests for the application

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,13 +2,9 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const { missions } = useSelector((state) => state.missions);
-<<<<<<< HEAD
-  const missionsJoined = missions.filter((mission) => mission.reserved);
-=======
   const { rockets } = useSelector((state) => state.rockets);
   const missionsJoined = missions.filter((mission) => mission.reserved);
   const bookedRockets = rockets.filter((rocket) => rocket.reserved);
->>>>>>> feature/rocketdata
 
   return (
     <div className="max-w-6xl mx-auto">
@@ -31,23 +27,6 @@ const Profile = () => {
           <h1 className="font-bold">Rockets</h1>
           <table className="table-fixed border dark:border-neutral-500 mt-4 mb-4">
             <tbody>
-<<<<<<< HEAD
-              <tr>
-                <td className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
-                  Good view
-                </td>
-              </tr>
-              <tr>
-                <td className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
-                  Good view again
-                </td>
-              </tr>
-              <tr>
-                <td className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
-                  Good view yet again
-                </td>
-              </tr>
-=======
               {bookedRockets.map((rocket) => (
                 <tr key={rocket.id}>
                   <td className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
@@ -55,7 +34,6 @@ const Profile = () => {
                   </td>
                 </tr>
               ))}
->>>>>>> feature/rocketdata
             </tbody>
           </table>
         </div>

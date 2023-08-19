52c12b90aa73c9e9d8d6e1aead6a3bac20e244db
mathsoft29 <iwuconcept@gmail.com>
2023-08-19 10:13:03 +0100

    Add thr rocketsBadges

--- src/components/Rocketslist.jsx
+++ src/components/Rocketslist.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import '../styles/style.css';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { bookRocket, cancelRocket } from '../Redux/Rockets/rocketSlice';
 
-const Rocketslist = ({ rockets }) => (
-  <div className="test">
-    <img className="img" src={rockets.images} alt="" />
-    <div className="info">
-      <h1>{rockets.name}</h1>
-
-      <p>{rockets.description}</p>
-      <button className="btn" type="button">reserved</button>
+const Rocketslist = ({ rockets }) => {
+  const dispatch = useDispatch();
+  const rocketBooking = () => {
+    if (rockets && rockets.reserved) {
+      dispatch(cancelRocket(rockets.id));
+    } else {
+      dispatch(bookRocket(rockets.id));
+    }
+  };
+  const switchApp = (reserved) => (reserved ? 'Cancel Reservation' : 'Book Rocket');
+  const badgeSwitch = (reserved) => (reserved ? 'Reserved' : '');
+  return (
+    <div className="test">
+      <img className="img" src={rockets.images} alt="" />
+      <div className="info">
+        <h1>{rockets.name}</h1>
+        <span className="btnn">{badgeSwitch(rockets?.reserved)}</span>
+        <p>{rockets.description}</p>
+        <button className="btn" type="button" onClick={rocketBooking}>{switchApp(rockets?.reserved)}</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 Rocketslist.propTypes = {
   rockets: PropTypes.shape().isRequired,
 };
